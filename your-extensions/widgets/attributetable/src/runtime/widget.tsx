41232e4bebd169badf764f1bab413f36635301d2
pwamly <pwamly2016@gmail.com>
2022-11-24 23:52:08 +0300

    deleting graphic layer from attribute table

--- your-extensions/widgets/attributetable/src/runtime/widget.tsx
+++ your-extensions/widgets/attributetable/src/runtime/widget.tsx
@@ -1,48 +1,67 @@
- /** @jsx jsx */
- import { React, AllWidgetProps, jsx } from "jimu-core";
- import { JimuMapViewComponent, JimuMapView } from "jimu-arcgis";
- import Point from "esri/geometry/Point";
+/** @jsx jsx */
+import { React, AllWidgetProps, jsx } from "jimu-core";
+import { JimuMapViewComponent, JimuMapView } from "jimu-arcgis";
+import Point from "esri/geometry/Point";
 
- export default class Widget extends React.PureComponent<AllWidgetProps<any>, any> {
-  state = {
-    jimuMapView: null,
-    //*** ADD ***//
-    latitude: "",
-    longitude: ""
-  };
-
- activeViewChangeHandler = (jmv: JimuMapView) => {
-  if (jmv) {
-    this.setState({
-      jimuMapView: jmv
-    });
-
-    jmv.view.on("pointer-move", (evt) => {
-  const point: Point = this.state.jimuMapView.view.toMap({
-    x: evt.x,
-    y: evt.y
-  });
-  this.setState({
-    latitude: point.latitude.toFixed(3),
-    longitude: point.longitude.toFixed(3)
-  });
-});
+export default class Widget extends React.PureComponent<
+  AllWidgetProps<any>,
+  any
+> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deletegraphics: null,
+      showWidget: false,
+    };
   }
-};
 
-   render() {
-     return  <div className="widget-starter jimu-widget">
-       {/* *** ADD *** */}
-       {this.props.hasOwnProperty("useMapWidgetIds") &&
-         this.props.useMapWidgetIds &&
-         this.props.useMapWidgetIds[0] && (
-           <JimuMapViewComponent
-             useMapWidgetId={this.props.useMapWidgetIds?.[0]}
-             onActiveViewChange={this.activeViewChangeHandler}
-           />
-         )
-       }
-        <p>Lat/Lon: {this.state.latitude} {this.state.longitude}</p>
-     </div>;
-   }
- }
+  componentDidMount(): void {}
+
+  componentDidUpdate(
+    prevProps: Readonly<AllWidgetProps<any>>,
+    prevState: Readonly<any>,
+    snapshot?: any
+  ): void {
+    if (this.props.hasOwnProperty("stateProps")) {
+      console.log("", this.props.stateProps.deletegraphics);
+      this.setState({ deletegraphics: this.props.stateProps.deletegraphics, showWidget: true });
+    }
+  }
+
+  render() {
+    return (
+      <div className="widget-starter jimu-widget">
+        {/* *** ADD *** */}
+        {this.props.hasOwnProperty("useMapWidgetIds") &&
+          this.props.useMapWidgetIds &&
+          this.props.useMapWidgetIds[0] && (
+            <JimuMapViewComponent
+              useMapWidgetId={this.props.useMapWidgetIds?.[0]}
+            />
+          )}
+        <h4>Attribute Table</h4>
+        {this.state.showWidget && (
+          <div> <table>
+          <tr style={{background:'gray'}}>
+            <th>Spatial Item</th>
+            <th>Data</th>
+          </tr>
+          <tr>
+            <td style={{fontWeight:'bold'}}>Graphic Id :</td>
+            <td></td>
+          </tr>
+          <tr>
+            <td style={{fontWeight:'bold'}}>Spatial details(l/w):</td>
+            <td></td>
+          </tr>
+         
+        </table>
+        
+          <button style={{background:'blue',color:'white'}} onClick={() => this.state.deletegraphics()}>
+            Delete
+          </button></div>
+        )}
+      </div>
+    );
+  }
+}
--- your-extensions/widgets/drawwidgets/src/runtime/widget.tsx
+++ your-extensions/widgets/drawwidgets/src/runtime/widget.tsx
@@ -1,72 +1,95 @@
-
-import {React,jsx,AllWidgetProps, WidgetManager, appActions} from 'jimu-core'
-import { JimuMapViewComponent,JimuMapView } from 'jimu-arcgis'
+import {
+  React,
+  jsx,
+  AllWidgetProps,
+  WidgetManager,
+  appActions,
+} from "jimu-core";
+import { JimuMapViewComponent, JimuMapView } from "jimu-arcgis";
 import Sketch from "@arcgis/core/widgets/Sketch";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import MapView from "@arcgis/core/views/MapView";
 import Map from "@arcgis/core/Map";
 
-export default class DrawingWidget extends React.PureComponent<AllWidgetProps<any>,any>{
-    
-    sketch = null;
-    graphicsLayer=null
+export default class DrawingWidget extends React.PureComponent<
+  AllWidgetProps<any>,
+  any
+> {
+  sketch = null;
+  graphicsLayer = null;
+  _init() {
+    this.graphicsLayer = new GraphicsLayer();
+    const map = new Map({
+      basemap: "topo-vector",
+      layers: [this.graphicsLayer],
+    });
 
-    _init(){
-        this.graphicsLayer = new GraphicsLayer();
-        const map = new Map({
-            basemap:"topo-vector",
-            layers:[this.graphicsLayer],
-        })
+    const view = new MapView({
+      container: "display_map",
+      map: map,
+      zoom: 5,
+      center: [90, 45],
+    });
 
- 
+    view.when(() => {
+      const sketch = new Sketch({
+        layer: this.graphicsLayer,
+        view: view,
+        creationMode: "update",
+      });
+      view.ui.add(sketch, "top-right");
+      sketch.on("create", (event) => {
+        if (event.state === "complete") {
+          console.log(event.graphic.geometry.toJSON());
+          this.openOff(event.graphic.geometry.toJSON());
+          
+          console.log('',event);
+        }
+      });
 
-        const view = new MapView({
-            container:"display_map",
-            map:map,
-            zoom:5,
-            center:[90,45],
-            
-        })
+      // listen to select event on the drawings
+      sketch.on("update", (event) => {
+        if (event.state === "start") {
+            function deletegraphics(){
+                sketch.delete();
+            }
+            console.log('',event);
 
-        view.when(()=>{
-            const sketch = new Sketch({
-                layer:this.graphicsLayer,
-                view:view,
-                creationMode:"update"
-            })
-            view.ui.add(sketch,"top-right");
-            sketch.on("create",(event)=>{
-                if(event.state === "complete"){
-                  console.log(event.graphic.geometry.toJSON());
-                  // console.log(webMercatorUtils.webMercatorToGeographic(event.graphic.geometry).toJSON());
-                  this.openOff(event.graphic.geometry.toJSON());
-                
-                }
-              
-            })
-        })
-    }
+          this.openAttrt(deletegraphics);
+        }
+      });
+    });
+  }
 
-    openOff=(data={})=>{
-      this.props.dispatch(appActions.widgetStatePropChange("widget_3","spatialData",data));
-    }
+  openOff = (data = {}) => {
+    this.props.dispatch(
+      appActions.widgetStatePropChange("widget_3", "spatialData", data)
+    );
+  };
 
-    componentDidMount(): void {
-        this._init();
-    }
+  openAttrt = (data) => {
+    this.props.dispatch(
+      appActions.widgetStatePropChange("widget_5", "deletegraphics", data)
+    );
+  };
 
-    render(): React.ReactNode {
-        return (
-            <div 
-                id = "display_map" 
-                style = {{   
-                    padding: 0,
-                    margin:0,
-                    height:900,
-                    width: "100%"
-                }}
-            >
-            </div>
-        )
-    }
+  componentDidMount(): void {
+    this._init();
+  }
+
+  render(): React.ReactNode {
+    return (
+      <div
+        id="display_map"
+        style={{
+          padding: 0,
+          margin: 0,
+          height: 900,
+          width: "100%",
+        }}
+      >
+        <div></div>
+      </div>
+    );
+  }
 }
